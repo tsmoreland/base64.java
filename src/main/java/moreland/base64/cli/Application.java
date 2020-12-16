2aaca18784f152d4db21b8cdd35216f3c340c40d
Terry Moreland <tsmoreland@live.com>
2020-12-16 00:20:06 +0000

    based infrastructure setup - logging and basic app settings for spring boot

--- src/main/java/moreland/base64/cli/Application.java
+++ src/main/java/moreland/base64/cli/Application.java
@@ -1,13 +1,25 @@
 package moreland.base64.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("provided arguments: {}", args.length);
+
 	}
 
 }
